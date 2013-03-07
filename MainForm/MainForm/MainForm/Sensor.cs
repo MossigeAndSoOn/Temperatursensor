46b76e352d38a977cf8bd1fe7fabbc318d1dc4d8
fredrik <studiefredfredrik@gmail.com>
2013-03-07 12:25:22 +0100

    Lagt til Sensor class for lesing av sensor
    Men mangler driverfilene (de ligger på USB sensoren) trenger å importere
    de til prosjektet for klassen fungerer.

---
+++ MainForm/MainForm/MainForm/Sensor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NationalInstruments;
+using NationalInstruments.DAQmx;
+
+namespace MainForm
+{
+    class Sensor
+    {
+        public static decimal GetTemp()
+        {
+            try
+            {
+                Task temperatureTask = new Task();
+                AIChannel myAIChannel;
+                myAIChannel = temperatureTask.AIChannels.CreateThermocoupleChannel(
+                "Dev1/ai0",
+                "Temperature",
+                0,
+                100,
+                AIThermocoupleType.J,
+                AITemperatureUnits.DegreesC,
+                25
+                );
+                AnalogSingleChannelReader reader = new
+                AnalogSingleChannelReader(temperatureTask.Stream);
+                double analogDataIn = reader.ReadSingleSample();
+                return Convert.ToDecimal(analogDataIn.ToString("0.0"));
+            }
+            catch (Exception ex)
+            {
+                Error.WriteLog("Sensor", ex.Message, "GetTemp failed");
+            }
+        }
+    }
+}
